dd0a0044e8c2b12c2a2d77064d36e63c65fb82a3
aboud20-meet <aboud20@meet.mit.edu>
2019-01-17 19:55:42 +0200

    message

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
-from flask import Flask
+from flask import *
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World test!'
+def home_page():
+    return render_template("home.html")
+
+
+@app.route('/join_band')
+def join_band():
+	return render_template('join_band.html')
+
+@app.route('/sign_in')
+def sign_in():
+	return render_template('sign_in.html')
+
+@app.route('/sign_up')
+def sign_up():
+	return render_template('sign up.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
-
+	app.run(debug=True)
